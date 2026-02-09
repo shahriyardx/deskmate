c558b2291bde2eb96e6abb82d4b3c548d59c3503
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2026-02-09 20:02:14 +0600

    added stage

---
+++ src/components/stage.tsx
@@ -0,0 +1,7 @@
+import React, { ComponentProps } from "react";
+
+const Stage = ({ children }: ComponentProps<"div">) => {
+  return <div className="w-full h-screen overflow-hidden">{children}</div>;
+};
+
+export default Stage;
