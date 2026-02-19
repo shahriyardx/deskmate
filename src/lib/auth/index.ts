38ebd9e49a134106a8c59bb1d7eed00804ef4717
Shahriyar Alam <mdshahriyaralam552@gmail.com>
2026-02-19 23:58:28 +0600

    updated env.example

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -6,8 +6,8 @@ import { prisma } from "../db"
 export const auth = betterAuth({
   socialProviders: {
     github: {
-      clientId: process.env.AUTH_CLIENT_ID as string,
-      clientSecret: process.env.AUTH_CLIENT_SECRET as string,
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET as string,
     },
   },
   database: prismaAdapter(prisma, { provider: "postgresql" }),
