ab00f08a7341f86e61aebcaea1dbc3b7e0bd0f60
Shahriyar Alam <mdshahriyaralam552@gmail.com>
2026-02-20 15:06:28 +0600

    Added pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Deskmate" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next"
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    start_url: "/",
+    name: "Deskmate",
+    short_name: "Deskmate",
+    description: "A minimal task management app",
+    icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    theme_color: "#000000",
+    background_color: "#000000",
+    display: "standalone",
+  }
+}
